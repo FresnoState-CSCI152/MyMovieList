420590e26176c956b67b34c5c2763f3cab30048f
Steven Nguyen <43051495+stevennguyenthanh@users.noreply.github.com>
2019-04-06 23:25:17 -0700

    Update post.blade.php

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -14,7 +14,7 @@
 	</div>
 	
 	{{ html_entity_decode(strip_tags(str_limit($post->body))) }} 
-	<!--{{ str_limit($post->body) }} string limit has add to the line above--> 
+	<!--{{ str_limit($post->body) }} string limit has add to the line above line 16--> 
 	
 	
 
