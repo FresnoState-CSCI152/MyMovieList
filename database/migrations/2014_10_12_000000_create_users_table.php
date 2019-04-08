250a3036306cf019b47990b88dd83f3d7f7e6ab8
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2019-04-07 18:36:48 -0700

    Updating User profile
    -- Added new entries in database to store user birthday, about me, gender, location
    -- small changes to layout of user profile

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('avatar')->default('default.jpg');
+            $table->string('birthday');
+            $table->string('gender');
+            $table->string('about_me');
+            $table->string('location');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -4,12 +4,29 @@
 
 	<div class ="container">
 		<div class="row">
-			<div> 
+			<div class="col-sm-4"> 
 				<h2> {{ $user->name }}'s Profile </h2>
 				<hr>
-				<img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+		    </div>
+		</div>
 
-                     <div class="col-md">
+			<div class="row">
+				<div class="col-sm-2">
+				<img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+			    </div>
+
+			    <!-- simple button to enable users to edit their profile pic -->
+			    <div class="col-xs-1">
+			    <div data-toggle="tooltip" title="Click to edit Image" style="color: grey">
+			    <ion-icon name = "add-circle" class="col-xs img-responsive" id = 'editButton' 
+			              onclick="showImageForm()" 
+			              style="width:40px;height:40px;cursor:pointer;">
+			              	
+			    </ion-icon>
+			    </div>
+			    </div>
+
+	  			    <div class="col-md-3" style="display:none" id="imgForm">
                          <div class="card">
                              <div class="card-body">
                              	<form enctype="multipart/form-data" action="/profile" method="POST">
@@ -20,16 +37,28 @@
             					</form>
                              </div>
                          </div>
-                     </div>
+                </div>
 			</div>
 		</div>
-	</div>
 
 	<hr>
+	<!--
 	<div class ="container">
 		<div class="row">
 			<a class="btn btn-primary" href="/friends" role="button">Friends</a>
 		</div>
 	</div>
+    -->
 
+<script>
+function showImageForm()
+{
+	var editProfileImg = $('#imgForm');
+	var editBtn = $('#editButton');
+
+	editProfileImg.show();
+	editBtn.hide();
+}
+
+</script>
 @endsection
