ca19a96a7cb4ddf9da160a4638ce98f3f82bd7ca
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2019-04-14 23:32:28 -0700

    Added post.php

---
+++ resources/views/posts/post.blade.php
@@ -0,0 +1,29 @@
+<div class="blog-post">
+	<h2 class="blog-post-title">
+		<a href="/discussion/{{ $post->id }}">{{ $post->title }}</a>
+	</h2>
+	<div class="row">
+		<div class="col- ml-3">
+        	<img src="/uploads/avatars/{{ $post->user->avatar }}" style="width:px; height:32px; position:relative; 	border-radius:50%">
+    	</div>	
+    	<div class="col d-flex align-items-center">
+    		<p class="blog-post-meta">
+    			<a href="/public/{{ $post->user->id }}">{{ $post->user->name }}</a> on {{ $post->created_at->tz('America/Los_Angeles')->toFormattedDateString() }}
+			</p>
+    	</div>
+	</div>
+	
+	{{ html_entity_decode(strip_tags(str_limit($post->body))) }} 
+	<!--{{ str_limit($post->body) }} string limit has add to the line above line 16--> 
+	
+	
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+	{{ html_entity_decode(strip_tags($post->body)) }}
+=======
+>>>>>>> master
+=======
+>>>>>>> master
+</div>
+<hr>
