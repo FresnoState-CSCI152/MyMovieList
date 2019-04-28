307787a93bd94522ab34863360e1a61d5e983afc
Steven Nguyen <43051495+stevennguyenthanh@users.noreply.github.com>
2019-04-28 16:42:03 -0700

    Added Cancel button for Post Create and Post Edit
    Added Cancel button for Create Post. If user change their mind and don't wanna upload their post, they just click Cancel and it will link back to Discussions page.
    Same thing on Edit.

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -21,6 +21,7 @@
 		
 			<div class="form-group">
 				<button type="submit" class="btn btn-primary">Update Post</button>
+				<a class="btn btn-primary" href="/discussion" role="button">Cancel</a>
 			</div>			
 
 			@include('errors/errors')
